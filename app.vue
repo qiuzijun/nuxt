0bfbabe683fbd9daaaab1ad415fe9b7d75545986
qiuzijun <84916951+qiuzijun@users.noreply.github.com>
2024-07-26 17:46:13 +0800

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-screen flex items-center justify-center">
+    <div
+      class="w-[600px] max-w-[600px] max-h-[400px] h-[400px]  bg-white rounded-lg p-4 flex flex-col items-end justify-center px-16">
+      <div class="w-32 h-32 md:w-36 md:h-36 bg-neutral-400 rounded-full blur-2xl"></div>
+      <div class="w-full h-full flex flex-col gap-4">
+        <div class="text-4xl text-left self-start font-black">钉锤</div>
+        <div class="gap-2">
+          <div class="text-3xl">手拿铁锤的人,</div>
+          <div class="text-3xl">看什麼都像钉子。</div>
+        </div>
+        <div class="gap-2">
+          <div class="text-lg">The person holding a hammer,</div>
+          <div class="text-lg">Everything looks like a nail.</div>
+        </div>
+        <div class="text-xl flex justify-end">—— 查理·芒格</div>
+      </div>
+    </div>
+  </div>
+</template>
