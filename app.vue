94cdbaded4dd1a6e2853f31092c216f1d4de12ea
qiuzijun <3118897593@qq.com>
2024-07-31 16:12:35 +0800

    feat

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
+<script setup lang="ts">
+const isOpen = ref(false);
+const colorMode = useColorMode()
+const isDark = computed({
+  get() {
+    return colorMode.value === 'dark'
+  },
+  set() {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
+
+</script>
 <template>
-  <div class="h-screen flex items-center justify-center">
+  <div class="h-screen flex items-center justify-center relative">
+    <div class="w-full p-40 pt-10 pb-10 flex justify-end items-center gap-x-5 absolute top-0">
+      <div class="text-sm cursor-pointer transition-all hover:text-primary-500" @click="isOpen = true">
+        个人简历</div>
+      <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray" variant="ghost"
+        aria-label="Theme" @click="isDark = !isDark" />
+    </div>
     <div
-      class="w-[600px] max-w-[600px] max-h-[400px] h-[400px]  bg-white rounded-lg p-4 flex flex-col items-end justify-center px-16">
+      class="w-[600px] max-w-[600px] max-h-[400px] h-[400px rounded-lg p-4 flex flex-col items-end justify-center px-16">
       <div class="w-32 h-32 md:w-36 md:h-36 bg-neutral-400 rounded-full blur-2xl"></div>
       <div class="w-full h-full flex flex-col gap-4">
         <div class="text-4xl text-left self-start font-black">学习</div>
@@ -17,4 +36,29 @@
       </div>
     </div>
   </div>
+  <UModal v-model="isOpen" fullscreen>
+    <UCard :ui="{
+        base: 'h-full flex flex-col',
+        rounded: '',
+        divide: 'divide-y divide-gray-100 dark:divide-gray-800',
+        body: {
+          base: 'flex-1 overflow-y-scroll'
+        }
+      }">
+      <template #header>
+        <div class="flex items-center justify-between">
+          <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
+            个人简历
+          </h3>
+          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+            @click="isOpen = false" />
+        </div>
+      </template>
+      <div class="h-max flex justify-center items-center ">
+        <div class="w-2/3 min-h-full">
+          <Resume />
+        </div>
+      </div>
+    </UCard>
+  </UModal>
 </template>
---
+++ components/Resume.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="flex justify-between items-center">
+        <!-- 基本信息 -->
+        <div class="h-40 flex flex-col justify-between">
+            <div class="flex flex-col gap-3">
+                <div class="text-1xl md:text-2xl font-semibold transition-all">嘿嘿嘿</div>
+                <div class="text-xs md:text-sm font-semibold transition-all">求职意向:嘿嘿嘿</div>
+            </div>
+            <div class="grid grid-cols-1 gap-1 gap-x-5 md:grid-cols-2">
+                <div class="text-xs md:text-sm flex items-center gap-1">
+                    <UIcon name="i-heroicons-light-bulb" class="w-3 h-3 md:w-4 md:h-4" />
+                    <span>出生年月：2024-09-09</span>
+                </div>
+                <div class="text-xs md:text-sm flex items-center gap-1">
+                    <UIcon name="i-heroicons-light-bulb" class="w-3 h-3 md:w-4 md:h-4" />
+                    <span>年龄：43</span>
+                </div>
+                <div class="text-xs md:text-sm flex items-center gap-1">
+                    <UIcon name="i-heroicons-light-bulb" class="w-3 h-3 md:w-4 md:h-4" />
+                    <span>电话：17770329870</span>
+                </div>
+                <div class="text-xs md:text-sm flex items-center gap-1">
+                    <UIcon name="i-heroicons-light-bulb" class="w-3 h-3 md:w-4 md:h-4" />
+                    <span>邮箱：17770329870@163.com</span>
+                </div>
+            </div>
+        </div>
+        <!-- 头像 -->
+        <div class="w-32 h-40 bg-primary-200 border-2 border-primary-50"></div>
+    </div>
+</template>
