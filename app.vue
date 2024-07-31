cb6b07263663927238fd1d6675e1e3c2eecab4cf
qiuzijun <3118897593@qq.com>
2024-07-31 10:09:34 +0800

    update

--- app.vue
+++ app.vue
@@ -4,16 +4,16 @@
       class="w-[600px] max-w-[600px] max-h-[400px] h-[400px]  bg-white rounded-lg p-4 flex flex-col items-end justify-center px-16">
       <div class="w-32 h-32 md:w-36 md:h-36 bg-neutral-400 rounded-full blur-2xl"></div>
       <div class="w-full h-full flex flex-col gap-4">
-        <div class="text-4xl text-left self-start font-black">钉锤</div>
+        <div class="text-4xl text-left self-start font-black">学习</div>
         <div class="gap-2">
-          <div class="text-3xl">手拿铁锤的人,</div>
-          <div class="text-3xl">看什麼都像钉子。</div>
+          <div class="text-3xl">人生得一知己足矣，</div>
+          <div class="text-3xl">斯世当以同怀视之。</div>
         </div>
         <div class="gap-2">
-          <div class="text-lg">The person holding a hammer,</div>
-          <div class="text-lg">Everything looks like a nail.</div>
+          <div class="text-lg">Having a confidant in life is enough,</div>
+          <div class="text-lg">Si Shi should view it with the same heart.</div>
         </div>
-        <div class="text-xl flex justify-end">—— 查理·芒格</div>
+        <div class="text-xl flex justify-end">——鲁迅</div>
       </div>
     </div>
   </div>
